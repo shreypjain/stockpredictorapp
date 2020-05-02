c7bc5e4df57b0a74d87745e2a1ce82c46d1e96e5
sjain23 <47904393+sjain23@users.noreply.github.com>
2020-05-02 01:36:57 -0400

    Update server.py

--- server.py
+++ server.py
@@ -3,8 +3,15 @@ app = Flask(__name__)
 
 @app.route('/stockquery')
 def func():
+    #So to test this on local host: "127.0.0.1:5000/stockquery?stockticker=stockTickerNameHere" and then you'll send 
     stockTicker = request.args.get('stockticker') 
     return jsonify({'ticker' : stockTicker})
+
+@app.route('/response')
+def func2():
+    #SHASHANK, this is gonna be where I need your code, to get the array and the graph passed
+    #as whatever var its gonna be passed as
+    return {'stockPrices' : [], 'graph' :  11}
     
 
 if __name__ == '__main__':
