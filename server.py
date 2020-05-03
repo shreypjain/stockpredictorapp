978c8ab2fadadcf5c54c4ff6d5a17837c635b7e5
sjain23 <47904393+sjain23@users.noreply.github.com>
2020-05-03 02:14:58 -0400

    updated

--- server.py
+++ server.py
@@ -1,18 +1,23 @@
-from flask import Flask, request, jsonify 
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  2 21:19:39 2020
+
+@author: shreyjain
+"""
+
+from flask import Flask, request
+import requests
+
 app = Flask(__name__)
 
-@app.route('/stockquery')
+@app.route('/', methods=['GET'])
 def func():
-    #So to test this on local host: "127.0.0.1:5000/stockquery?stockticker=stockTickerNameHere" and then you'll send 
-    stockTicker = request.args.get('stockticker') 
-    return jsonify({'ticker' : stockTicker})
+    ticker = request.args['ticker']
+    jsonObj = {'ticker' : ticker}
+    r = requests.get(url = 'http://127.0.0.1:8030/?ticker='+str(ticker))
+    return r
 
-@app.route('/response')
-def func2():
-    #SHASHANK, this is gonna be where I need your code, to get the array and the graph passed
-    #as whatever var its gonna be passed as
-    return {'stockPrices' : [], 'graph' :  11}
-    
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port = 5000, debug=True)
+    
